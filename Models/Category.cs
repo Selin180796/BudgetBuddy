32a6bd33ccaa153650715bc77f15388f26119493
Daryll Christian <dchrist8@mycambrian.ca>
2024-08-01 16:48:46 -0400

    Added Category model to Models directory

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BudgetBuddy.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Optional: Navigation property for related expenses
+        public ICollection<Expense> Expenses { get; set; }
+    }
+}
