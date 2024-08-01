abead2d1d7767a3302656f16e77ffe9f762b2ae9
Daryll Christian <dchrist8@mycambrian.ca>
2024-08-01 17:19:13 -0400

    Added Transaction  model to Models directory

---
+++ Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BudgetBuddy.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+
+        // Foreign key to the User model
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        // Foreign key to the Category model
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
+
