a66b0848d9cbcdcd0ac13143fbae12ba82ecf4b7
Selin <schrist9@mycambrian.ca>
2024-08-01 16:22:56 -0400

    Added User model to Models directory

---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BudgetBuddy.Models
+{
+    public class User
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        // Constructor
+        public User(string firstName, string lastName, string email, DateTime dateOfBirth, string address, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            DateOfBirth = dateOfBirth;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
+
